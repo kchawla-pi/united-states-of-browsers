4ddc0fccad0addcc34342cedf3b20317f2bf14df
kchawla-pi <kc.insight.pi@gmail.com>
2017-09-20 19:27:34 -0400

    Refactored: merge_db.py code into functions

--- src/merge_db.py
+++ src/merge_db.py
@@ -16,29 +16,44 @@ def create_test_data():
 	return test_record
 
 
-test_record = create_test_data()
-test_record.update({field: str(value) for field, value in test_record.items() if value is None})
-field_names = ', '.join([str(field) for field in test_record.keys()])
-data = list(test_record.values())
+def preprocess_record(record):
+	record.update({field: str(value) for field, value in record.items() if value is None})
+	field_names = ', '.join([str(field) for field in record.keys()])
+	data = list(record.values())
+	return field_names, data
 
+
+def make_queries(table_name, field_names, values):
+	queries = {'create': '''CREATE TABLE {} ({})'''.format(table_name, field_names)}
+	queries.update({'insert': "INSERT INTO {} VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)".format(table_name)})
+	return queries
+
+
+def create_table(cursor):
+	try:
+		cursor.execute(queries['create'])
+	except sqlite3.OperationalError as excep:
+		print(excep)
+
+
+def insert_record(cursor, data):
+	cursor.execute(queries['insert'], data)
+	conn.commit()
+
+
+test_record = create_test_data()
+field_names, data = preprocess_record(test_record)
+table_name = 'history'
+queries = make_queries(table_name, field_names, values=data)
 
 print(field_names)
-table_name = 'history'
-query_create = '''CREATE TABLE {} ({})'''.format(table_name, field_names)
-query_insert = "INSERT INTO {} VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)".format(table_name)
+
 conn, cur, filepath = db_handler.connect_db('test.sqlite')
-try:
-	cur.execute(query_create)
-except sqlite3.OperationalError as excep:
-	print(excep)
 
-cur.execute(query_insert, data)
+create_table(cursor=cur)
+insert_record(cursor=cur,data=data)
+record_yielder = record_fetcher.yield_prepped_records(cursor=cur, table=table_name, filepath=filepath)
 
-conn.commit()
-# cur.execute('''SELECT * FROM {}'''.format(table_name))
-# print(cur.description)
-
-record_yielder = record_fetcher.yield_prepped_records(cursor=cur, table='history', filepath=filepath)
 for record_ in record_yielder:
 	print(record_)
 	
