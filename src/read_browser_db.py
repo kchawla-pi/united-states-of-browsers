1449d9e17ec88798afd2d0a4e8785f46caab5b38
kchawla-pi <kc.insight.pi@gmail.com>
2017-09-24 02:41:56 -0400

    Improved print_records()

--- src/read_browser_db.py
+++ src/read_browser_db.py
@@ -1,40 +1,52 @@
 import sqlite3
 
+import browser_setup
 import db_handler
 import record_fetcher
-import browser_setup
+import reorganizer
 
-def print_records(record_gen, each_time=10):
+def print_records(record_gen, each_time=10, profile_name=None):
 	import time
+	if profile_name:
+		print('\n' * 2, profile_name, '\n' * 2)
+	time.sleep(2)
 	print('Press ENTER key for the next set of records, c for number of records so far, any other to exit.\n\n')
 	time.sleep(1)
 	for srn, record in enumerate(record_gen):
 		print(record)
-		if srn % each_time == 0 and srn > 0:
-			quitter = input()
-			if quitter in {'c', 'C'}:
-				print(srn)
-				time.sleep(1)
-			elif quitter:
-				break
+		try:
+			cond = srn % each_time == 0 and srn > 0
+		except TypeError:
 			pass
-			
+		else:
+			if cond:
+				quitter = input()
+				if quitter in {'c', 'C'}:
+					print(srn)
+					time.sleep(1)
+				elif quitter:
+					break
+				pass
+				
 	
 def firefox(profiles=None):
 	profile_paths = browser_setup.setup_profile_paths(browser_name_or_path='firefox',
 													  profiles=profiles)
-	file_paths = browser_setup.db_filepath(root=profile_paths, filenames='places', ext='sqlite')
-	for idx, file_ in enumerate(file_paths):
+	file_paths = browser_setup.db_filepath(profile_paths=profile_paths, filenames='places', ext='sqlite')
+	for idx, profile_name_ in enumerate(file_paths):
 		tables = ['moz_places']
 		for table_ in tables:
 			try:
-				conn, cur, filename = db_handler.connect_db(db_file=file_)
+				conn, cur, filename = db_handler.connect_db(db_file=file_paths[profile_name_])
 			except sqlite3.OperationalError as excep:
 				print(excep)
 			else:
 				prepped_records = record_fetcher.yield_prepped_records(cursor=cur, table=table_,
-				                                                       filepath=file_)
-				print_records(record_gen=prepped_records, each_time=10)
+				                                                       filepath=file_paths[profile_name_])
+				# yield prepped_records
+				print_records(record_gen=prepped_records, each_time='all', profile_name=profile_name_)
+				# all_url_hashes = reorganizer.deduplicate_sites(prepped_records)
+				# print(len(all_url_hashes))
 			finally:
 				cur.close()
 				conn.close()
@@ -45,12 +57,11 @@ def chrome():
 				root="C:\\Users\\kshit\\AppData\\Local\\Google\\Chrome\\User Data\\Default",
 				filenames='History', ext=None)
 	
-	"C:\\Users\\kshit\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History"
 
 
 if __name__ == '__main__':
 	profiles = ['RegularSurfing', 'default', 'dev-edition-default']
-	firefox('RegularSurfing')
+	firefox()
 # chrome()
 
 """
