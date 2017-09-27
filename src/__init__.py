16f1597d259d3242155f042598f2dc8abe1da2b5
kchawla-pi <kc.insight.pi@gmail.com>
2017-09-27 13:48:15 -0400

    Commented out contents of __init__.py, imports are working regardless

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,10 @@
-import pathlib
-search_in = pathlib.Path(__file__).parent
-
-from . import browser_setup
-from . import db_handler
-from . import deduplicator
-from . import read_browser_db
-from . import record_fetcher
-from . import write_new_db
-from .write_new_db import safetychecks
+# import pathlib
+# search_in = pathlib.Path(__file__).parent
+#
+# from . import browser_setup
+# from . import db_handler
+# from . import deduplicator
+# from . import read_browser_db
+# from . import record_fetcher
+# from . import write_new_db
+# from .write_new_db import safetychecks
