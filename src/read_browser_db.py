3a3e0881394d8cf141c30cdd129c624b4d81c5fe
kchawla-pi <kc.insight.pi@gmail.com>
2017-10-03 17:20:29 -0400

    Changed: Empty record dict is generated using known field names before DB access
     - Fieldnames extracted during DB access update the template, potentially
     may add new fields to it. (TODO)

--- src/read_browser_db.py
+++ src/read_browser_db.py
@@ -1,7 +1,10 @@
 import sqlite3
 
+from collections import OrderedDict as odict
+
 import browser_setup
 import db_handler
+import helpers
 import record_fetcher
 import write_new_db
 from show import quick_read_record
@@ -19,25 +22,41 @@ def firefox(profiles=None):
 	file_paths = browser_setup.db_filepath(profile_paths=profile_paths, filenames='places', ext='sqlite')
 	return file_paths
 
+
+def make_record_dict_template(cursor, table):
+	helpers.safetychecks(table)
+	query = '''SELECT * FROM {}'''.format(table)
+	try:
+		cursor.execute(query)
+	except sqlite3.OperationalError:
+		return Exception(table)
+	field_names = [desc_[0] for desc_ in cursor.description]
+	record_template = odict.fromkeys(field_names, None)
+	return record_template
 	
-def read_browser_db(filepaths):
+	
+def read_browser_database(filepaths):
+	fieldnames_template = ['id', 'url', 'title', 'rev_host', 'visit_count', 'hidden', 'typed',
+	                       'favicon_id', 'frecency', 'last_visit_date', 'guid', 'foreign_count',
+	                       'url_hash', 'description', 'preview_image_url',
+	                       ]
+	record_template = odict.fromkeys(fieldnames_template, None)
 	for idx, profile_name_ in enumerate(filepaths):
 		tables = ['moz_places']
 		for table_ in tables:
 			try:
-				conn, cur, filename = db_handler.connect_db(db_file=filepaths[profile_name_])
+				source_db_info = db_handler.connect_db(db_file=filepaths[profile_name_])
 			except sqlite3.OperationalError as excep:
 				print(excep)
 			else:
-				prepped_records = record_fetcher.yield_prepped_records(cursor=cur, table=table_,
-				                                                       filepath=filepaths[profile_name_]
+				prepped_records = record_fetcher.yield_prepped_records(cursor=source_db_info['cursor'], table=table_,
+				                                                       filepath=filepaths[profile_name_],
+				                                                       record_template=record_template,
 				                                                       )
 				yield prepped_records
-				
-
 			finally:
-				cur.close()
-				conn.close()
+				source_db_info['cursor'].close()
+				source_db_info['connection'].close()
 
 
 def chrome():
@@ -49,9 +68,14 @@ def chrome():
 if __name__ == '__main__':
 	profiles = ['RegularSurfing', 'default', 'dev-edition-default']
 	# firefox(['test_profile0'])
-	read_browser_db(firefox())
-	quit()
-	quick_read_record(database='test.sqlite')
+	filepaths = firefox('default')
+	yield_db = read_browser_database(filepaths)
+	for rec in yield_db:
+		for entry in rec:
+			print(entry)
+	# read_browser_database({'default': 'C:\\Users\\kshit\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\xl8257ca.default\\places.sqlite'})
+	# quit()
+	# quick_read_record(database='test.sqlite')
 
 # chrome()
 
