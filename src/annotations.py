baae943ae827e90849ba0d4a9953cc27aa62c45f
kchawla-pi <kc.insight.pi@gmail.com>
2017-10-04 15:35:32 -0400

    Added: Annotation typing related imports refactored into annotations.py

---
+++ src/annotations.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+
+from pathlib import Path
+from typing import (AnyStr,
+                    Dict,
+                    Generator,
+                    Iterable,
+                    List,
+                    Optional,
+                    Sequence,
+                    SupportsInt,
+                    Tuple, Type, TypeVar,
+                    Union,
+                    NewType,
+                    )
