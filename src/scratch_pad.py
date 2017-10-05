05e343d1fb94af1257dfe306fb336bec5002bb75
kchawla-pi <kc.insight.pi@gmail.com>
2017-10-05 14:16:30 -0400

    Moved: Example output files from /src/ to /outputs/

--- src/scratch_pad.py
+++ src/scratch_pad.py
@@ -5,3 +5,30 @@ keys = {
 
 	}
 
+import os
+filepath_from_another = lambda *filename, filepath=__file__: os.path.realpath(os.path.join(os.path.dirname(filepath), *filename))
+
+print(filepath_from_another('output', 'abc.txt'))
+print(filepath_from_another('output', 'try', 'abc.txt'))
+print(filepath_from_another('output', 'try', 'abc.txt', filepath='C:'))
+
+
+def fpfa(filepath=__file__, *filename):
+	filepath_dir = os.path.dirname(filepath)
+	filepath = os.path.join(*filepath)
+	return os.path.realpath(os.path.join(filepath_dir, filepath))
+	
+print()
+print(fpfa())
+print(fpfa('abc.txt'))
+print(fpfa('output', 'abc.txt'))
+print()
+
+filepath_from_another = lambda filepath=__file__, *filename: os.path.realpath(os.path.join(os.path.dirname(filepath), *tuple(filename)))
+
+# print(filepath_from_another())
+print(filepath_from_another('abc.txt'))
+print(filepath_from_another('output', 'abc.txt'))
+# print(filepath_from_another('output', 'try', 'abc.txt', filepath='C:'))
+
+
