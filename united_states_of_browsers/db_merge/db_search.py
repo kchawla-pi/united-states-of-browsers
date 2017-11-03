3113ac426ddbd6aff4cc0f74cc2258ff38dfa954
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2017-11-03 14:13:28 -0400

    Refactored: run_search(), run_search_long() into separate functions
     - Added:
        . make_sql_statements()
        . print_search()
     - Changed:
        . run_search()
     - Removed:
        . run_search_long()
     - Tightened up some docstrings.
     - Added annotations.

--- united_states_of_browsers/db_merge/db_search.py
+++ united_states_of_browsers/db_merge/db_search.py
@@ -2,13 +2,14 @@ import datetime
 import json
 import re
 import sqlite3
-from collections import (namedtuple,
-                         )
+
+from collections import namedtuple
 
 from united_states_of_browsers.db_merge import database_operations as db_ops
 from united_states_of_browsers.db_merge.paths_setup import app_inf_path
 from united_states_of_browsers.db_merge.imported_annotations import *
 
+from pprint import pprint
 
 search_table_fieldnames = ('id', 'url', 'title', 'visit_count',
                            'last_visit_date', 'url_hash', 'description')
@@ -16,15 +17,11 @@ search_table_fieldnames = ('id', 'url', 'title', 'visit_count',
 with open(app_inf_path, 'r') as json_obj:
 	app_inf = json.load(json_obj)
 
-# SearchRecord = namedtuple('SearchRecord', app_inf['search_table_fields'])
 DBRecord = namedtuple('DBRecord', app_inf['source_fieldnames'])
 
 def build_search_table(db_path: PathInfo, included_fieldnames: Sequence[Text]):
 	""" Builds virtual table for full-text search in sqlite databases.
 	Accepts path to the sqlite file and subset of its fieldnames to be included in the virtual table.
-	
-	db_path: Path to the SQLite Database file. (str, bytestring, Path, PathLike)
-	included_fieldnames: Ordered subset of the database's fieldnames to be included in the search table.
 	"""
 	with sqlite3.connect(db_path) as sink_conn:
 		column_str = ', '.join(included_fieldnames)
@@ -60,55 +57,53 @@ def create_search_query(any_word, all_words=None, not_words=None):
 	return words_any, words_all, words_not
 
 
-# print(repr(words_any))
-# print(repr(words_all))
-# print(repr(words_not))
-
-
-def run_search(db_path, search_query):
-	# db_path.get(
-	with sqlite3.connect(db_path) as sink_conn:
-		query_results = sink_conn.execute(
-				"""SELECT *
-				FROM search_table
-				WHERE search_table
-				MATCH ?""", [search_query]
-				)
-		for result in query_results:
-			print(result)
-
-
-def run_search_long(db_path, word_query, date_start=None, date_stop=None):
-	# db_path.get(
-	sql_query = ('SELECT *'
-	             ' FROM moz_places'
+def make_sql_statement(word_query: Text,
+                       date_start: Optional[int]=None, date_stop: Optional[int]=None
+                       ) -> Union[Text, Iterable[Text]]:
+	""" Returns prepared SQL statements and bindings for queries with and without dates.
+	Accepts word_query.
+		Optional: date_start and date_stop.
+	"""
+	if date_start and date_stop:
+		query_addon = ' AND last_visit_date BETWEEN ? AND ?'
+		query_bindings = [word_query, date_start, date_stop]
+	else:
+		query_addon = ''
+		query_bindings = [word_query]
+	sql_query = ('SELECT * FROM moz_places'
 	             ' WHERE id IN'
 	             ' (SELECT id'
 	             ' FROM search_table'
 	             ' WHERE search_table'
-	             ' MATCH ?)'
-	             ' AND last_visit_date'
-	             ' BETWEEN ? AND ?')
-	# pp = pprint.PrettyPrinter(indent=4, compact=False)
+	             f' MATCH ?){query_addon}'
+	             )
+	return sql_query, query_bindings
+
+
+def run_search(db_path: PathInfo, sql_query: Text, query_bindings: Iterable[Text]) -> Iterable[NamedTuple]:
+	
 	with sqlite3.connect(db_path) as sink_conn:
 		sink_conn.row_factory = sqlite3.Row
-		query_results = sink_conn.execute(sql_query, [word_query, date_start, date_stop])
-		for result in query_results:
-			search_record = DBRecord(*result)
-			timestamp_ = search_record.last_visit_date
+		query_results = sink_conn.execute(sql_query, query_bindings)
+		return [DBRecord(*result) for result in query_results]
+		
+		
+def print_search(search_results: Iterable):
+	for result in search_results:
+		timestamp_ = result.last_visit_date
+		try:
 			human_readable_date = datetime.datetime.utcfromtimestamp(timestamp_ / 10 ** 6)
-			# pp.pprint(DBRecord(*result).title)
-			print(human_readable_date, '.', search_record.title)
-			# pprint([human_readable_date, search_record.title, search_record.url])
-			# pprint(DBRecord(*result))
-	
-
-
+		except TypeError as excep:
+			human_readable_date = 'Date Unavailable'
+		if not result.title:
+			print(human_readable_date, '.', result.url)
+		else:
+			print(human_readable_date, '.', result.title)
+			
+		
 def build_new_search_table():
 	with open('app_inf.json', 'r') as json_obj:
 		app_inf = json.load(json_obj)
-	# included_fieldnames = (
-	# 'url', 'title', 'visit_count', 'last_visit_date', 'url_hash', 'description')
 	build_search_table(db_path=app_inf['sink'], included_fieldnames=app_inf['search_table_fields'])
 
 
@@ -117,12 +112,14 @@ if __name__ == '__main__':
 	human_times = [datetime.datetime.utcfromtimestamp(timestamp_ / 10 ** 6) for timestamp_ in
 	               time_stamps]
 	
-	# pprint(human_times[1:])
-	# run_search_long(db_path=app_inf['sink'], word_query='python', date_start=time_stamps[1],
-	#                 date_stop=time_stamps[2])
-	# pprint(human_times[1:])
+	sql_query, query_bindings = make_sql_statement(word_query='python', date_start=time_stamps[1], date_stop=time_stamps[2])
+	run_search(db_path=app_inf['sink'], sql_query=sql_query, query_bindings=query_bindings)
+	print()
+	search_query = "python AND variable NOT update anaconda AND stackoverflow"
+	sql_query, query_bindings = make_sql_statement(word_query=search_query)
+	search_results = run_search(db_path=app_inf['sink'], sql_query=sql_query, query_bindings=query_bindings)
+	print_search(search_results)
 	
-	run_search_long(db_path=app_inf['sink'], word_query='python', date_start=time_stamps[1])
 	"""
 	entered_searches, search_queries = get_searches_list()
 	for query in entered_searches:
@@ -138,8 +135,6 @@ if __name__ == '__main__':
 	# pprint(app_inf)
 	# search_query = 'title:python'
 	
-	search_query = "python AND variable NOT update anaconda AND stackoverflow"
-	run_search(db_path=app_inf['sink'], search_query=search_query)
 	# print(datetime.datetime.utcfromtimestamp(1509123590555000/10**6))
 	# run_search_long(path_info=app_inf['sink'], word_query='python', )
 	"""
