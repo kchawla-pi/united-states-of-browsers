590b35e04783b153a5adfcc9350e4545adec1079
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2017-12-08 21:13:26 -0500

    Browser() now stores the complete information including table info (for Firefox)
     - sqlite3.connection, cursor are returned by _connect, not bound to
     Browser instance.
     - Browser()[attribute_name] now returns attribute value (resembles Mapping).
     - self.db_paths is now self.db_info .
     - db_info contains all data from sqlite_master table, browser, file,
     profile, path.

--- united_states_of_browsers/oops/browser_setup.py
+++ united_states_of_browsers/oops/browser_setup.py
@@ -12,37 +12,49 @@ from united_states_of_browsers.db_merge.imported_annotations import *
 
 raise_exceps = 0
 
-class Browser:
-	def __init__(self, profile_pathcrumbs: Union[Iterable[AnyStr], PathInfo], database_files:Iterable[Text]):
+class Browser(dict):
+	def __init__(self, browser, profile_pathcrumbs: Union[Iterable[AnyStr], PathInfo], database_files:Iterable[Text]):
 		""" Accepts the browser name and/or a dict of pathrcrumbs for it.
 		Figures out the current platform and chooses pathcrumbs accordingly.
 		"""
+		self.browser = browser
 		self.pathcrumbs = profile_pathcrumbs
 		self.files = database_files
 		self.os = sys.platform
-		self.db_paths = None
+		self.db_info = None
+		self.connection = None
 		self.cursor = None
+		self.tables = {}
+	#
+	def __getitem__(self, item):
+		return self.__dict__[item]
+
+	def keys(self):
+		return self.__dict__.keys()
 
 	def setup_paths(self) -> Iterable[Dict[Tuple[Text, Text], PathInfo]]:
 		profile_root = Path(*self.pathcrumbs).expanduser()
 		profile_paths = {profile.name.split(sep='.', maxsplit=1)[1]: profile for profile in profile_root.iterdir()}
-		self.db_paths = {(profile, file): path.joinpath(file) for profile, path in profile_paths.items() for file in self.files}
+		self.db_info = [{'browser': self.browser, 'profile': profile, 'file': file, 'path': path.joinpath(file)} for profile, path in profile_paths.items() for file in self.files]
 		# return db_paths
 
 	def _connect(self, path) -> Generator:
 		connection_arg = f'file:{path}?mode=ro'
 		with sqlite3.connect(connection_arg, uri=True) as connection:
 			connection.row_factory = sqlite3.Row
-			cursor = connection.cursor()
-			return cursor
+			return connection.cursor()
+
 
 	def _get_table_info(self, cursor):
 		query = 'SELECT * FROM sqlite_master;'
 		cursor.execute(query)
-		return cursor
+		return [dict(table_entry) for table_entry in cursor]
 
-	def read_table(self):
-		for (profile, file), path in self.db_paths.items():
+		# self.table.update({'browser': self.browser, 'file': file})
+
+	def read_table_info(self):
+		for idx, entry in enumerate(self.db_info):
+			profile, file, path = entry['profile'], entry['file'], entry['path']
 			try:
 				cursor = self._connect(path)
 			except sqlite3.OperationalError as excep:
@@ -53,21 +65,31 @@ class Browser:
 						print(f'\nFAILED: Browser._connect().\n'
 						      f'Connecting to {path} failed.\n'
 						      f'File {file} does not exist for {profile}.\n'
-						      f'Moving on...\n'
+						      f'Removing erring entries and moving on...\n'
 						      )
+						self.db_info.pop(idx)
 				else:
 					raise sqlite3.OperationalError(excep)
 			else:
-				table_info = self._get_table_info(cursor)
-				pprint(table_info.fetchone())
+				tables = self._get_table_info(cursor)
+				entry.update({'tables': tables})
 		pass
 
 if __name__ == '__main__':
-	browser_info = profile_pathcrumbs_firefox = ['~', 'AppData', 'Roaming', 'Mozilla', 'Firefox', 'Profiles']
+	profile_pathcrumbs_firefox = ['~', 'AppData', 'Roaming', 'Mozilla', 'Firefox', 'Profiles']
 	chrome_path = f'~\\AppData\\Local\\Google\\Chrome\\User Data\\Default'
+	chrome_path = f'~\\AppData\\Local\\Google\\Chrome\\User Data'
 
-	firefox = Browser(profile_pathcrumbs_firefox, ['places.sqlite', 'favicons.sqlite', 'arbit'])
+	firefox = Browser('firefox', profile_pathcrumbs_firefox, ['places.sqlite', 'favicons.sqlite', 'arbit'])
 	firefox.setup_paths()
-	firefox.read_table()
+	# pprint(firefox.db_info)
+	firefox.read_table_info()
+	chrome = Browser('chrome', [chrome_path], ['history'])
+	print(chrome)
+	# chrome.setup_paths()
+	# print(firefox['browser'])
+	# print(firefox['pathcrumbs'])
+	# print(firefox['browser'])
+	# print(firefox.__dict__.keys())
 
 # temp_path = db_paths[('test_profile0', 'arbit')]; temp_conn = f'file:{temp_path}?mode=ro'; temp_gen = self._connect(temp_path); print(next(temp_gen, None))
