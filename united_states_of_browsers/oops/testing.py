0cecf4a42e795da301ab757d9931f42078f81665
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2018-01-02 06:53:31 +0530

    Removed existing files from /oops for a fresh start

--- united_states_of_browsers/oops/testing.py
+++ united_states_of_browsers/oops/testing.py
@@ -1,5 +1,7 @@
+# -*- encoding: utf-8 -*-
+from pprint import pprint
 
-from oops.browser_setup import Browser
+from oops.new_browser_setup import Browser
 from oops.database_file import DBFile
 from oops.database_table import Table
 
@@ -7,8 +9,9 @@ def fx_test():
 	profile_pathcrumbs_firefox = ['~', 'AppData', 'Roaming', 'Mozilla', 'Firefox', 'Profiles']
 	firefox = Browser('firefox', profile_pathcrumbs_firefox, ['places.sqlite'])
 	firefox.setup_paths()
-	print(firefox)
-
+	pprint(firefox.profile_paths)
+	current_path = firefox.profile_paths
+	# db_file0 = DBFile(current_path)
 
 if __name__ == '__main__':
 	fx_test()
