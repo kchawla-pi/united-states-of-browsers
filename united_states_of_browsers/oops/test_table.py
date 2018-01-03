b633a6765f400cba096c984d66c712b67960c69e
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2018-01-03 06:54:55 +0530

    Added tests for Table.make_record() for Chrome
     - refactored test_case tuple -> test_case_in, test_case_expected

--- united_states_of_browsers/oops/test_table.py
+++ united_states_of_browsers/oops/test_table.py
@@ -6,13 +6,30 @@ from united_states_of_browsers.oops import table
 from united_states_of_browsers.oops import test_table_data as ttd
 
 
-@pytest.mark.parametrize('test_case', [test_case for test_case in zip(ttd.init_testdata_input, ttd.init_testdata_expected)])
-def test_firefox_init(test_case):
-	actual_output = table.Table(*test_case[0])
-	assert actual_output == test_case[1]
+@pytest.mark.parametrize(('test_case_in', 'test_case_expected'), [(test_case_in, test_case_expected)
+                                                                  for test_case_in, test_case_expected
+                                                                  in zip(ttd.init_testdata_input, ttd.init_testdata_expected)
+                                                                  ])
+def test_init(test_case_in, test_case_expected):
+	actual_output = table.Table(*test_case_in)
+	assert actual_output == test_case_expected
 
-@pytest.mark.parametrize('test_case', [test_case for test_case in zip(ttd.make_records_testdata_input, ttd.make_records_testdata_expected)])
-def test_firefox_make_records(test_case):
-	test_case[0].get_records()
-	actual_output = [dict(record)['url'] for record in test_case[0].records_yielder]
-	assert actual_output == ttd.make_records_testdata_expected
+
+@pytest.mark.parametrize(('test_case_in', 'test_case_expected'), [(test_case_in, test_case_expected)
+                                                                  for test_case_in, test_case_expected
+                                                                  in zip(ttd.make_records_testdata_input, ttd.make_records_testdata_expected)
+                                                                  ])
+def test_make_records(test_case_in, test_case_expected):
+	test_case_in.get_records()
+	actual_output = set(dict(record)['url'] for record in test_case_in.records_yielder)
+	assert actual_output == test_case_expected
+
+def test_whiteboard():
+	for test_case in zip(ttd.make_records_testdata_input, ttd.make_records_testdata_expected):
+		print(test_case[0])
+		test_case[0].get_records()
+		actual = set(dict(record)['url'] for record in test_case[0].records_yielder)
+		pprint(actual)
+		print(test_case[1])
+
+# test_whiteboard()
--- united_states_of_browsers/oops/test_table_data.py
+++ united_states_of_browsers/oops/test_table_data.py
@@ -5,6 +5,7 @@ from united_states_of_browsers.oops.table import Table
 TableData = namedtuple('TableData','table, path, browser, file, profile')
 
 firefox_profile_path = 'C:\\Users\\kshit\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\'
+chrome_profile_path = 'C:\\Users\\kshit\\AppData\\Local\\Google\\Chrome\\User Data\\'
 init_testdata_input = (
 	TableData(table='moz_places',
 	          path=firefox_profile_path+'kceyj748.test_profile1\\places.sqlite',
@@ -29,6 +30,12 @@ init_testdata_input = (
 	          browser='firefox',
 	          file='places.sqlite',
 	          profile='default',
+	          ),
+	TableData(table='urls',
+	          path=chrome_profile_path+'Profile 1\\History',
+	          browser='chrome',
+	          file='history',
+	          profile='Profile 1',
 	          ),
 	)
 
@@ -57,18 +64,30 @@ init_testdata_expected = [
 	          file='places.sqlite',
 	          profile='default',
 	          ),
+	Table(table='urls',
+	          path=chrome_profile_path+'Profile 1\\History',
+	          browser='chrome',
+	          file='history',
+	          profile='Profile 1',
+	          ),
 	]
 
 make_records_testdata_input = [
-		Table(table='moz_places',
-	          path=firefox_profile_path+'kceyj748.test_profile1\\places.sqlite',
-	          browser='firefox',
-	          file='places.sqlite',
-	          profile='test_profile1',
-	          ),
+	Table(table='moz_places',
+		path=firefox_profile_path+'kceyj748.test_profile1\\places.sqlite',
+		browser='firefox',
+		file='places.sqlite',
+		profile='test_profile1',
+		),
+	Table(table='urls',
+		path=chrome_profile_path+'Profile 1\\History',
+		browser='chrome',
+		file='history',
+		profile='Profile 1',
+		),
 	]
-make_records_testdata_expected = [
-	'place:sort=8&maxResults=10',
+make_records_testdata_expected = (
+	set(['place:sort=8&maxResults=10',
 	'place:type=6&sort=14&maxResults=10',
 	'https://www.mozilla.org/privacy/firefox/',
 	'https://www.nytimes.com/2017/10/20/opinion/sunday/to-complain-is-to-truly-be-alive.html',
@@ -80,4 +99,28 @@ make_records_testdata_expected = [
 	'place:folder=TOOLBAR',
 	'place:folder=BOOKMARKS_MENU',
 	'place:folder=UNFILED_BOOKMARKS',
-	]
+	    ]),
+	set(['https://www.google.com/search?q=test&oq=test&aqs=chrome..69i57j0l5.789j0j7&sourceid=chrome&ie=UTF-8',
+	'http://www.gmail.com/',
+	'https://www.gmail.com/',
+	'https://www.google.com/gmail/',
+	'https://mail.google.com/mail/',
+	'https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=https://mail.google.com/mail/&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1#',
+	'https://mail.google.com/intl/en/mail/help/about.html#',
+	'https://www.google.com/intl/en/mail/help/about.html#',
+	'https://www.google.com/gmail/about/#',
+	'https://www.google.com/search?q=village+dental+nyc&oq=village+dental+nyc&aqs=chrome..69i57j0l5.2878j0j7&sourceid=chrome&ie=UTF-8',
+	'http://www.villagedentalnyc.com/',
+	'https://www.ident.ws/template_include/new_patient_sign_in.do?site=10679&practiceId=37810',
+	'https://www.ident.ws/template_include/new_patient_forms.do?FirstName=Kshitij&MI=&LastName=Chawla&apptDate=12%2F12%2F2017',
+	'https://www.ident.ws/template_include/questionnaire_form.do?id=2785850972',
+	'http://www.yahoo.com/',
+	'https://www.yahoo.com/',
+	'https://www.ident.ws/template_include/cal.jsp?format=MM%2FDD%2FYYYY&datefield=document.forms.signInForm.apptDate',
+	'https://coderpad.io/ZARYRHRP',
+	'https://mail.google.com/mail/u/0/#inbox',
+	'https://accounts.google.com/AccountChooser?service=mail&continue=https://mail.google.com/mail/',
+	'https://accounts.google.com/ServiceLogin?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1',
+	'https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin',
+	    ]),
+	)
