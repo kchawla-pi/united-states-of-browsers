097f7e4441726a6a2a402eeef8f77ba3f873d7db
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2018-01-05 21:54:58 +0530

    Added functions to assist in exception logging and cleanup
     - oops.exceptions_handling.remove_new_empty_files()
     - oops.exceptions_handling.exceptions_log_deduplicator()

--- united_states_of_browsers/oops/exceptions_handling.py
+++ united_states_of_browsers/oops/exceptions_handling.py
@@ -5,8 +5,21 @@ def invalid_path_in_tree(path_to_test):
 	""" Accepts a path and returns the first invalid parent.
 	"""
 	path_to_test = Path(path_to_test)
-	first_invalid_path_in_tree = [path_parent for path_parent in path_to_test.parents if not path_parent.exists()]
+	first_invalid_path_in_tree = [path_parent for path_parent in path_to_test.parents if not path_parent.exists() or not path_parent.is_dir()]
 	return first_invalid_path_in_tree[-1] if first_invalid_path_in_tree else None
+
+
+def remove_new_empty_files(dirpath, existing_files):
+	dirpath = Path(dirpath)
+	files_post_connection_attempt = set(entry for entry in dirpath.iterdir() if entry.is_file())
+	extra_files = files_post_connection_attempt.difference(existing_files)
+	[file_.unlink() for file_ in extra_files if file_.stat().st_size == 0]
+
+
+def exceptions_log_deduplicator(exceptions_log):
+	unique_exception_strings = {str(excep_): excep_ for excep_ in exceptions_log}
+	return list(unique_exception_strings.values())
+
 
 
 def test_path_tester():
