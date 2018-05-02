ccf5211169bb8db4d86248605fb5fe231ea7f265
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2018-05-02 17:15:15 -0400

    Added invalid path exception test cases in test_os_error.py

--- tests/test_table/test_os_error_exception.py
+++ tests/test_table/test_os_error_exception.py
@@ -12,16 +12,16 @@ TableArgs = namedtuple('TableArgs', 'table path browser filename profile copies_
 test_cases_exception_no_such_table = [
 	TableArgs(table='moz_places',
 	          path=Path(project_root,
-	                    'tests/data/browser_profiles_for_testing/AppData/Roaming/Mozilla/'
-	                    'Firefox/Profiles/udd5sttq.test_profile2/moz_places.sqlite'),
+	                    'tests/data/browser_profiles_for_testing_wrongpath_/AppData/Roaming/Mozilla/'
+	                    'Firefox/Profiles/kceyj748.test_profile1/places.sqlite'),
 	          browser='firefox',
 	          filename='non_db_dummy_file_for_testing.txt',
-	          profile='test_profile2',
+	          profile='test_profile1',
 	          copies_subpath=None,
 	          ),
 	TableArgs(table='urls',
 	          path=Path(project_root,
-	                    'tests/data/browser_profiles_for_testing/AppData/Local/Google/Chrome/User Data/Profile 1/History_not_real'),
+	                    'tests/data/browser_profiles_for_testing/AppData/Local/Google/Chrome/User Data/Profile 1 _wrongpath_/History'),
 	          browser='chrome',
 	          filename='History',
 	          profile='Profile 1',
@@ -29,7 +29,7 @@ test_cases_exception_no_such_table = [
 	          ),
 	TableArgs(table='urls',
 	          path=Path(project_root,
-	                    'tests/data/browser_profiles_for_testing/AppData/Local/Vivaldi/User Data/Default/History'),
+	                    'tests/data/browser_profiles_for_testing/AppData/Local/Vivaldi/User_wrongpath_ Data/Default/History'),
 	          browser='vivaldi',
 	          filename='History',
 	          profile='Default',
@@ -37,7 +37,7 @@ test_cases_exception_no_such_table = [
 	          ),
 	TableArgs(table='urls',
 	          path=Path(project_root,
-	                    'tests/data/browser_profiles_for_testing/AppData/Roaming/Opera Software/Opera Stable/History'),
+	                    'tests/data/browser_profiles_for_testing/AppData_wrongpath_/Roaming/Opera Software/Opera Stable/History'),
 	          browser='opera',
 	          filename='History',
 	          profile='Opera Stable',
@@ -49,23 +49,24 @@ test_cases_exception_no_such_table = [
 @pytest.mark.parametrize('test_case', [test_case for test_case in test_cases_exception_no_such_table])
 def test_suite_os_error(test_case):
 	table_obj = Table(*test_case)
-	with pytest.raises(sqlite3.DatabaseError) as excep:
+	with pytest.raises(OSError) as excep:
 		table_obj.get_records()
 
 
 def non_pytest_test_suite_os_error():
 	for test_case in test_cases_exception_no_such_table:
 		table_obj = Table(*test_case)
-		table_obj.get_records()
-		# try:
-		# 	pass
-		# except sqlite3.DatabaseError as excep:
-		# 	print(excep, '--', test_case.browser, test_case.profile, test_case.filename, test_case.table)
-		# else:
-		# 	print('No error.', '--', test_case.browser, test_case.profile, test_case.filename, test_case.table)
-		# finally:
-		# 	print()
+		try:
+			table_obj.get_records()
+			pass
+		except OSError as excep:
+			print(f'Expected Exception raised', excep, '--', test_case.browser, test_case.profile, test_case.filename, test_case.table)
+		else:
+			print(f'Expected Exception NOT raised', '--', test_case.browser, test_case.profile, test_case.filename, test_case.table)
+		finally:
+			print()
+		# break
 			# assert str(excep) == 'file is not a database'
 
 if __name__ == '__main__':
-	run_non_pytests()
+	non_pytest_test_suite_os_error()
