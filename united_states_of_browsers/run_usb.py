dc7c6d37041ec00d10435a0420809b60c2064d5d
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2018-05-01 20:13:44 -0400

    Fixed module run_usb.py to merge & run USB

--- united_states_of_browsers/run_usb.py
+++ united_states_of_browsers/run_usb.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 import webbrowser
 
@@ -8,71 +7,30 @@ from pathlib import Path
 def make_paths():
 	venv_python_path = Path(__file__).absolute().parents[1]
 	venv_python_path = venv_python_path.joinpath('venv', 'Scripts', 'python.exe')
-	run_usb_path = Path(__file__).absolute().parent
-	return venv_python_path, run_usb_path
-	
-	
-def make_command_module(venv_python_path, run_usb_path, module_to_be_imported, function_to_be_run):
-	launch_command = f'{venv_python_path} -m {run_usb_path}'
-	
-	
-def make_command(venv_python_path, run_usb_path, module_to_be_imported, function_to_be_run):
-	launch_command = ' '.join(
-			[str(venv_python_path), '-c',
-			 f'"import {module_to_be_imported}; {module_to_be_imported}.{function_to_be_run}()"'])
-	return launch_command
+	usb_app_path = Path(__file__).absolute().parent
+	db_merge_module_path = usb_app_path.joinpath('db_merge', 'orchestrator.py')
+	usb_server_module_path = usb_app_path.joinpath('usb_server', 'usb_server.py')
+	return venv_python_path, db_merge_module_path, usb_server_module_path
 
-def launch_usb_mock():
-	print('launched')
-	quit()
-	
-	
-# def merge_browsers():
-# 	from united_states_of_browsers.db_merge.orchestrator import merge_browsers
-# 	print('Merging browser databases...')
-# 	merge_browsers()
-#
-# def run_flask():
-# 	from united_states_of_browsers.usb_server.usb_server import run_flask
-# 	print('Starting browser interface...')
-# 	run_flask()
-# 	usb_flask_url = 'http://127.0.0.1:5000/'
-# 	webbrowser.open_new_tab(url=usb_flask_url)
 
-#
-# def launch_usb():
-# 	merge_browsers()
-# 	run_flask()
+def make_command_module(venv_python_path, db_merge_module_path, usb_server_module_path):
+	db_merge_command = f'{venv_python_path} {db_merge_module_path}'
+	usb_server_command = f'{venv_python_path} {usb_server_module_path}'
+	return db_merge_command, usb_server_command
 
-def main():
-	venv_python_path, run_usb_path = make_paths()
-	launch_browser_merge = make_command_module(venv_python_path,
-	                                    run_usb_path,
-	                                    module_to_be_imported='run_usb',
-	                                    function_to_be_run='merge_browsers',
-	                                    )
-	print(locals())
+
+def launch_usb():
+	venv_python_path, db_merge_module_path, usb_server_module_path = make_paths()
+	db_merge_command, usb_server_command = make_command_module(venv_python_path,
+	                                                           db_merge_module_path,
+	                                                           usb_server_module_path,
+	                                                           )
+	subprocess.run(db_merge_command)
+	subprocess.run(usb_server_command)
 	
-	
-	
-	
-	# launch_ui = make_command(venv_python_path,
-	#                          run_usb_path,
-	#                          module_to_be_imported='run_usb',
-	#                          function_to_be_run='run_flask',
-	#                          )
-	# curr_dir = os.getcwd()
-	# os.chdir(run_usb_path)
-	# subprocess.run(launch_browser_merge)
-	# subprocess.run(launch_ui)
-	# os.chdir(curr_dir)
+	# usb_ui_url = 'http://127.0.0.1:5000/'
+	# webbrowser.open_new_tab(usb_ui_url)
 
 
 if __name__ == '__main__':
-	main()
-	# venv_python_path, run_usb_path = make_paths()
-	# curr_dir = os.getcwd()
-	# os.chdir(run_usb_path)
-	# subprocess.run('C:\\Users\\kshit\\OneDrive\\workspace\\UnitedStatesOfBrowsers\\venv\\Scripts\\python.exe -c '
-	#                '"import run_usb; run_usb.launch_usb()"',
-	#                )
+	launch_usb()
