fe69a00495fe769ebebd54edf33f4e43d0e77468
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-08-29 14:26:53 -0400

    Simplified tests; added dummy db files

--- tests/unit_tests/test_table/test_InvalidFileError_exception.py
+++ tests/unit_tests/test_table/test_InvalidFileError_exception.py
@@ -5,13 +5,13 @@ from pathlib import Path
 from tests.fixtures import get_project_root_path
 from united_states_of_browsers.db_merge.table import Table
 from united_states_of_browsers.db_merge.custom_exceptions import InvalidFileError
-from united_states_of_browsers.db_merge import helpers
 
 
 project_root = get_project_root_path()
-TableArgs = namedtuple('TableArgs', 'table path browser filename profile copies_subpath')
-test_cases_exception_InvalidFileError = [
-	TableArgs(table='moz_places',
+
+
+def test_InvalidFileError_mozilla_1():
+	table_obj = Table(table='moz_places',
 	          path=Path(project_root,
 	                    'tests/data/browser_profiles_for_testing/AppData/Roaming/Mozilla/'
 	                    'Firefox/Profiles/udd5sttq.test_profile2/non_db_dummy_file_for_testing.txt'),
@@ -19,8 +19,13 @@ test_cases_exception_InvalidFileError = [
 	          filename='non_db_dummy_file_for_testing.txt',
 	          profile='test_profile2',
 	          copies_subpath=None,
-	          ),
-	TableArgs(table='moz_places',
+	          )
+	with pytest.raises(InvalidFileError):
+		table_obj.make_records_yielder()
+
+	
+def test_InvalidFileError_mozilla_2():
+	table_obj = Table(table='moz_places',
 	          path=Path(project_root,
 	                    'tests/data/browser_profiles_for_testing/AppData/Roaming/Mozilla/'
 	                    'Firefox/Profiles/udd5sttq.test_profile2/places_non_existent.sqlite'),
@@ -28,86 +33,45 @@ test_cases_exception_InvalidFileError = [
 	          filename='places_non_existent.sqlite',
 	          profile='test_profile2',
 	          copies_subpath=None,
-	          ),
-	TableArgs(table='urls',
+	          )
+	with pytest.raises(InvalidFileError):
+		table_obj.make_records_yielder()
+
+	
+def test_InvalidFileError_chrome():
+	table_obj = Table(table='urls',
 	          path=Path(project_root,
 	                    'tests/data/browser_profiles_for_testing/AppData/Local/Google/Chrome/User Data/Profile 1/History_false_filename'),
 	          browser='chrome',
 	          filename='History_false_filename',
 	          profile='Profile 1',
 	          copies_subpath=None,
-	          ),
-	TableArgs(table='urls',
+	          )
+	with pytest.raises(InvalidFileError):
+		table_obj.make_records_yielder()
+
+	
+def test_InvalidFileError_vivaldi():
+	table_obj = Table(table='urls',
 	          path=Path(project_root,
 	                    'tests/data/browser_profiles_for_testing/AppData/Local/Vivaldi/User Data/Default/History_false_filename'),
 	          browser='vivaldi',
 	          filename='History',
 	          profile='Default',
 	          copies_subpath=None,
-	          ),
-	TableArgs(table='urls',
+	          )
+	with pytest.raises(InvalidFileError):
+		table_obj.make_records_yielder()
+
+
+def test_InvalidFileError_opera():
+	table_obj = Table(table='urls',
 	          path=Path(project_root,
 	                    'tests/data/browser_profiles_for_testing/AppData/Roaming/Opera Software/Opera Stable/History_false_filename'),
 	          browser='opera',
 	          filename='History',
 	          profile='Opera Stable',
 	          copies_subpath=None,
-	          ),
-	]
-
-
-@pytest.mark.parametrize('test_case', [test_case for test_case in test_cases_exception_InvalidFileError])
-def test_InvalidFileError(test_case):
-	table_obj = Table(*test_case)
+	          )
 	with pytest.raises(InvalidFileError):
 		table_obj.make_records_yielder()
-
-
-# def non_pytest_test_InvalidFileError(test_suite):
-# 	for test_case in test_suite:
-# 		table_obj = Table(*test_case)
-# 		try:
-# 			table_obj.make_records_yielder()
-# 		except InvalidFileError as excep:
-# 			print('Expected exception raised: InvalidFileError', excep, '--', test_case.browser, test_case.profile,
-# 			      test_case.filename, test_case.table)
-# 		else:
-# 			print('Expected exception InvalidFileError NOT raised: .', '--', test_case.browser, test_case.profile,
-# 			      test_case.filename, test_case.table)
-# 			raise Exception
-# 		finally:
-# 			print()
-#
-#
-# def simply_run(test_suite):
-# 	for test_case in test_suite:
-# 		table_obj = Table(*test_case)
-# 		table_obj.make_records_yielder()
-#
-#
-# if __name__ == '__main__':
-# 	non_pytest_test_InvalidFileError(test_suite=test_cases_exception_InvalidFileError)
-# 	quit()
-# 	# simply_run(test_suite=test_cases_exception_InvalidFileError)
-# 	fx_false_file = Table(table='moz_places',
-# 	                      path=Path(project_root,
-# 	                                'tests/data/browser_profiles_for_testing/AppData/Roaming/Mozilla/'
-# 	                                'Firefox/Profiles/udd5sttq.test_profile2/non_db_dummy_file_for_testing.txt'),
-# 	                      browser='firefox',
-# 	                      filename='non_db_dummy_file_for_testing.txt',
-# 	                      profile='test_profile2',
-# 	                      copies_subpath=None,
-# 	                      )
-#
-# 	# fx_false_file.make_records_yielder()
-#
-# 	chrome_false_file = Table(table='urls',
-# 	                          path=Path(project_root,
-# 	                                    'tests/data/browser_profiles_for_testing/AppData/Local/Google/Chrome/User Data/Profile 1/History_false_filename'),
-# 	                          browser='chrome',
-# 	                          filename='History_false_filename',
-# 	                          profile='Profile 1',
-# 	                          copies_subpath=None,
-# 	                          )
-# 	chrome_false_file.make_records_yielder()
-# 	print(list(chrome_false_file.records_yielder))
--- tests/unit_tests/test_table/test_InvalidTableError_exception.py
+++ tests/unit_tests/test_table/test_InvalidTableError_exception.py
@@ -10,9 +10,9 @@ from united_states_of_browsers.db_merge.custom_exceptions import InvalidTableErr
 
 project_root = get_project_root_path()
 
-TableArgs = namedtuple('TableArgs', 'table path browser filename profile copies_subpath')
-test_cases_exception_InvalidTableError = [
-	TableArgs(table='moz_places',
+
+def test_InvalidTableError_mozilla():
+	table_obj = Table(table='moz_places',
 	          path=Path(project_root,
 	                    'tests/data/browser_profiles_for_testing/AppData/Roaming/Mozilla/'
 	                    'Firefox/Profiles/udd5sttq.test_profile2/places.sqlite'),
@@ -20,59 +20,57 @@ test_cases_exception_InvalidTableError = [
 	          filename='places.sqlite',
 	          profile='test_profile2',
 	          copies_subpath=None,
-	          ),
-	TableArgs(table='nonexistent_table',
+	          )
+	with pytest.raises(InvalidTableError):
+		table_obj.make_records_yielder()
+
+	
+def test_InvalidTableError_chrome():
+	table_obj = Table(table='nonexistent_table',
 	          path=Path(project_root,
 	                    'tests/data/browser_profiles_for_testing/AppData/Local/Google/Chrome/User Data/Profile 1/History'),
 	          browser='chrome',
 	          filename='History',
 	          profile='Profile 1',
 	          copies_subpath=None,
-	          ),
-	TableArgs(table='nonexistent_table',
+	          )
+	with pytest.raises(InvalidTableError):
+		table_obj.make_records_yielder()
+
+	
+def test_InvalidTableError_vivaldi():
+	table_obj = Table(table='nonexistent_table',
 	          path=Path(project_root,
 	                    'tests/data/browser_profiles_for_testing/AppData/Local/Vivaldi/User Data/Default/History'),
 	          browser='vivaldi',
 	          filename='History',
 	          profile='Default',
 	          copies_subpath=None,
-	          ),
-	TableArgs(table='nonexistent_table',
+	          )
+	with pytest.raises(InvalidTableError):
+		table_obj.make_records_yielder()
+
+	
+def test_InvalidTableError_opera():
+	table_obj = Table(table='nonexistent_table',
 	          path=Path(project_root,
 	                    'tests/data/browser_profiles_for_testing/AppData/Roaming/Opera Software/Opera Stable/History'),
 	          browser='opera',
 	          filename='History',
 	          profile='Opera Stable',
 	          copies_subpath=None,
-	          ),
-	# TableArgs(table='moz_places',
-	#              path=Path(
-	#                     'tests/data/browser_profiles_for_testing/AppData/Roaming/Mozilla/'
-	#                     'Firefox/Profiles/e0pj4lec.test_profile0/places.sqlite'),
-	#              browser='firefox',
-	#              filename='places.sqlite',
-	#              profile='test_profile0',
-	#              copies_subpath=None,
-	#
-	#              ),
-	]
-
-
-@pytest.mark.parametrize('test_case', [test_case for test_case in test_cases_exception_InvalidTableError])
-def test_suite_no_such_table(test_case):
-	table_obj = Table(*test_case)
+	          )
 	with pytest.raises(InvalidTableError):
 		table_obj.make_records_yielder()
 
-
-def non_pytest_test_suite_no_such_table(test_suite):
-	for test_case in test_suite:
-		table_obj = Table(*test_case)
-		try:
-			table_obj.make_records_yielder()
-		except InvalidTableError:
-			print('Passed.')
-
-
-if __name__ == '__main__':
-	non_pytest_test_suite_no_such_table(test_suite=test_cases_exception_InvalidTableError)
+	
+# TableArgs(table='moz_places',
+#              path=Path(
+#                     'tests/data/browser_profiles_for_testing/AppData/Roaming/Mozilla/'
+#                     'Firefox/Profiles/e0pj4lec.test_profile0/places.sqlite'),
+#              browser='firefox',
+#              filename='places.sqlite',
+#              profile='test_profile0',
+#              copies_subpath=None,
+#
+#              ),
