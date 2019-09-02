28fecc4de55b417bd04a44e9a8aded4b34c3a62a
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-09-01 21:21:35 -0400

    Added test for Table.check_if_empty_db()

--- tests/unit_tests/test_table/test_no_exceptions.py
+++ tests/unit_tests/test_table/test_no_exceptions.py
@@ -1,3 +1,7 @@
+import sqlite3
+import tempfile
+from pathlib import Path
+
 from united_states_of_browsers.db_merge.table import Table
 
 
@@ -27,3 +31,29 @@ def test_suite_no_exceptions_mozilla(create_mozilla_data):
     table_obj.make_records_yielder()
     for entry in table_obj.records_yielder:
         entry
+
+
+def test_check_if_emoty_db(create_chromium_data):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        dbname = 'empty.sqlite'
+        dbpath = str(Path(tmpdir, dbname))
+        conn = sqlite3.connect(dbpath)
+        conn.close()
+        empty_ = Table(table='nonexistent_table',
+                      path=dbpath,
+                      browser=None,
+                      filename=dbname,
+                      profile=None,
+                      )
+        empty_._connect()
+        assert empty_.check_if_db_empty() == True
+        empty_._connection.close()
+        
+    not_empty_table = Table(table='urls',
+                            path=create_chromium_data,
+                            browser=None,
+                            filename='anything',
+                            profile=None,
+                            )
+    not_empty_table._connect()
+    assert not_empty_table.check_if_db_empty() == False
