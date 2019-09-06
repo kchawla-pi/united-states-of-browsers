276b755f2417afa47d8ca96eac38625806812f6a
Kshitij Chawla <kc.insight.pi@gmail.com>
2019-09-05 22:12:12 -0400

    Two more tests for all profiles in Chrome and Firefox (#26)
    * Added 2 tests for all profiles

--- tests/unit_tests/test_Browser_access_mozilla.py
+++ tests/unit_tests/test_Browser_access_mozilla.py
@@ -178,3 +178,53 @@ def test_browser_access_same_profile_file_two_tables(tests_root):
     
     assert moz_places_expected_ids == moz_places_actual_ids
     assert moz_origins_expected_ids == moz_origins_actual_ids
+
+
+def test_browser_access_all_profiles_same_file_table(tests_root):
+    profile_rootpath = Path(tests_root, 'firefox_databases')
+    browser_two_profiles = Browser(browser='firefox',
+                                   profiles=None,
+                                   profile_root=profile_rootpath,
+                                   )
+    browser_two_profiles.add_tables_for_access(file='places.sqlite',
+                                               tables=['moz_places'], )
+    moz_places_records_yielder = browser_two_profiles.access_fields(
+            table='moz_places',
+            fields=['id', 'url', 'title',
+                    'last_visit_date',
+                    'last_visit_readable',
+                    ]
+            
+            )
+    profile_1_2_records_using_browser = [record for record in
+                                         moz_places_records_yielder]
+    sort_by_id = lambda item: item['id']
+    profile_1_2_records_using_browser.sort(key=sort_by_id)
+    
+    profile1_table = Table(table='moz_places',
+                           path=Path(profile_rootpath,
+                                     't87e6f86.test_profile1',
+                                     'places.sqlite',
+                                     ),
+                           browser='mozilla',
+                           filename='places.sqlite',
+                           profile='test_profile1',
+                           )
+    profile1_table.make_records_yielder()
+    profile1_records = list(profile1_table.records_yielder)
+    
+    profile2_table = Table(table='moz_places',
+                           path=Path(profile_rootpath,
+                                     'z786c76dv78.test_profile2',
+                                     'places.sqlite',
+                                     ),
+                           browser='mozilla',
+                           filename='places.sqlite',
+                           profile='test_profile2',
+                           )
+    profile2_table.make_records_yielder()
+    profile2_records = list(profile2_table.records_yielder)
+    
+    profile_1_2_records_using_tables = [*profile1_records, *profile2_records]
+    profile_1_2_records_using_tables.sort(key=sort_by_id)
+    assert profile_1_2_records_using_tables == profile_1_2_records_using_tables
